6813f9b4bf6435f513d80c22f7a59e0c77b9f28d
rays <raystercarino@gmail.com>
2025-01-31 17:38:33 +0800

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -219,8 +219,8 @@ end
 local Window = Fluent:CreateWindow({
     Title = "Lucifer " .. CONFIG.LuciferVer,
     SubTitle = "Made by Haro",
-    TabWidth = 100,
-    Size = UDim2.fromOffset(100, 100),
+    TabWidth = 50,
+    Size = UDim2.fromOffset(50, 50),
     Acrylic = true,
     Theme = "Dark",
     MinimizeKey = Enum.KeyCode.LeftControl
