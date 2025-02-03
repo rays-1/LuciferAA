a91ce0e7d0a03a62300a009c456f2b0272371c88
rays <raystercarino@gmail.com>
2025-02-03 11:18:20 +0800

    a

--- macro.lua
+++ macro.lua
@@ -40,3 +40,57 @@ end
 printTable(equippedUnits)
 
 -- printTable(UnitsData)
+
+
+-- Define the RemoteFunctions
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local spawnUnitRemote = ReplicatedStorage.endpoints.client_to_server.spawn_unit
+local upgradeUnitRemote = ReplicatedStorage.endpoints.client_to_server.upgrade_unit_ingame
+local sellUnitRemote = ReplicatedStorage.endpoints.client_to_server.sell_unit_ingame
+
+local logArray = {}
+
+local function logArguments(remoteName, ...)
+    local args = {...} 
+    if remoteName == "upgrade_unit_ingame" or remoteName == "sell_unit_ingame" then
+        table.insert(logArray, {
+            Remote = remoteName,
+            Arguments = args,
+            etc = args[1]:GetAttribute("_SPAWN_UNIT_UUID")
+        })
+    else
+        table.insert(logArray, {
+            Remote = remoteName,
+            Arguments = args
+        })
+    end
+    print("["..remoteName.."] Invoked with arguments:", unpack(args))
+
+end
+
+local mt = getrawmetatable(game)
+local oldInvokeServer = mt.__namecall
+
+setreadonly(mt, false) 
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if method == "InvokeServer" then
+        if self == spawnUnitRemote then
+            logArguments("spawn_unit", ...)
+        elseif self == upgradeUnitRemote then
+            logArguments("upgrade_unit_ingame", ...)
+        elseif self == sellUnitRemote then
+            logArguments("sell_unit_ingame", ...)
+        end
+    end
+    return oldInvokeServer(self, ...)
+end)
+setreadonly(mt, true) 
+
+while true do
+    print("Log Array Contents:")
+    for i, entry in ipairs(logArray) do
+        print("Entry ["..i.."]: Remote = "..entry.Remote..", Arguments = "..unpack(entry.Arguments).." , etc: "..entry.args)
+    end
+    task.wait(1)
+end
